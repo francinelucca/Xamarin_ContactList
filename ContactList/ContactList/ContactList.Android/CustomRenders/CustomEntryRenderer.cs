54ed2d9603569d2e75f276be05b243d94ff2abcd
francinelucca <francinelucca@hotmail.com>
2020-02-23 14:09:16 -0400

    Custom Entry Renderers

---
+++ ContactList/ContactList/ContactList.Android/CustomRenders/CustomEntryRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ContactList.Droid.CustomRenders;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace ContactList.Droid.CustomRenders
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context)
+            : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null || e.NewElement == null) return;
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+                Control.BackgroundTintList = ColorStateList.ValueOf(Android.Graphics.Color.LightGray);
+            else
+                Control.Background.SetColorFilter(Android.Graphics.Color.LightGray, PorterDuff.Mode.SrcAtop);
+        }
+    }
+}
---
+++ ContactList/ContactList/ContactList.iOS/CustomRenders/CustomEntryRenderer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ContactList.iOS.CustomRenders;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CustomEntryRenderer))]
+namespace ContactList.iOS.CustomRenders
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        private CALayer _line;
+
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            _line = null;
+
+            if (Control == null || e.NewElement == null)
+                return;
+
+            Control.BorderStyle = UITextBorderStyle.None;
+
+            _line = new CALayer
+            {
+                BorderColor = UIColor.FromRGB(244, 244, 244).CGColor,
+                BackgroundColor = UIColor.FromRGB(244, 244, 244).CGColor,
+                Frame = new CGRect(0, Frame.Height / 2, Frame.Width + 40, 1f)
+            };
+
+            Control.Layer.AddSublayer(_line);
+        }
+    }
+}
